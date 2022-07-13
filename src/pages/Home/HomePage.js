a939a69c7346f8bb544da2963c3e97b6a547e4ee
Ayrton Teshima <ayrtonteshima@gmail.com>
2022-07-13 20:38:27 -0300

    aula 6 video 10

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,10 +6,18 @@ import { Card } from '../../components/Card/Card';
 import { ModalSavePin } from '../../containers/ModalSavePin/ModalSavePin';
 import { ModalCreateFolder } from '../../containers/ModalCreateFolder/ModalCreateFolder';
 
+const Teste = ({ label }) => {
+  return ReactDOM.createPortal(
+    <h4>{label}</h4>,
+    document.body
+  )
+};
+
 export const HomePage = () => {
   return (
     <div>
-      <ModalCreateFolder open={true} />
+      <Teste label="Oi, mundo" />
+      <ModalCreateFolder open={false} />
       <Container fluid>
         <Row>
           <Col xs={12} md={2}><Card title="Matemática" image="https://picsum.photos/200/300?53" total={0} /></Col>
